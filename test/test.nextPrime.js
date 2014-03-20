3c4420c90db3598911e348b8ec7ef64a1ef998dd
Vidya Raghavan <vidya@shippable.com>
2014-03-20 13:47:38 +0530

    Update test.nextPrime.js

--- test/test.nextPrime.js
+++ test/test.nextPrime.js
@@ -6,6 +6,11 @@ describe('nextPrime', function() {
   it('nextPrime should return the next prime number', function() {
     assert.equal(11, nextPrime(7));
   });
+  
+  it('nextPrime should return the next prime number', function() {
+    assert.equal(19, nextPrime(17));
+  });
+
 
   it('zero and one are not prime numbers', function() {
     assert.equal(2, nextPrime(0));
